aaf10c762846836c4861e824f8ff6ec5234f46a8
C1904H <164913030+C1904H@users.noreply.github.com>
2024-05-05 16:24:01 +0100

    created pokemonList variable land added objects to array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
-alert('Hello world');
+// New variable with blank array
+let pokemonList = [];
 
-let favoriteFood = 'Thai';
-document.write (favoriteFood);
+// Pokemon objects
+let pokemons = [
+  { name: 'Charmander', height: 6, types: ['fire','blaze'] },
+  { name: 'Squirtle', height: 5, types: ['water','torrent'] },
+  { name: 'Pikachu', height: 4, types: ['electric','static'] }
+];
+
+

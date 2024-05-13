dcba51b12ce6630d6ecfd2314f1543676a27f77e
C1904H <164913030+C1904H@users.noreply.github.com>
2024-05-13 17:40:58 +0100

    IIFE added to pokemonList array
    forEach loop updated and redundant code tidied up.

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,61 @@
-// Pokemon objects
-let pokemonList = [
-    { name: 'Charmander', height: 0.6, types: ['Fire', 'Blaze'] },
-    { name: 'Squirtle', height: 0.5, types: ['Water', 'Torrent'] },
-    { name: 'Pikachu', height: 0.4, types: ['Electric', 'Static'] }
-];
+// pokemonList array in IIFE
+let pokemonRepository = (function () {
 
-// loop for pokemonList (stating name and height)
-//for (let i=0; i < pokemons.length; i++) {
-//document.write("<p>" + pokemons[i].name + ' (height: ' + pokemons[i].height + ')' + "</p>");  
+    let pokemonList = [
+        {
+            name: 'Charmander',
+            height: 0.6,
+            types: ['Fire', ' Blaze']
+        },
+        {
+            name: 'Squirtle',
+            height: 0.5,
+            types: ['Water', ' Torrent']
+        },
+        {
+            name: 'Pikachu',
+            height: 0.4,
+            types: ['Electric', ' Static']
+        }
+    ];
 
-// chain conditional for pokemon height 
-//if (pokemons[i].height > 0.5) {
-    //document.write(' - Wow, that\'s big!!');
-//} else if (pokemons[i].height <= 0.5 && pokemons[i].height > 0.4) {
-   //document.write(' - That\'s average');
-//} else {
-    //document.write(' - That\'s small');
+    // Return pokemon array
+    function getAll() {
+        return pokemonList;
+    }
 
-//forEach() Loop for pokemon objects
-pokemonList.forEach(function (pokemon) {
+    // Add pokemon item to list
+    function add(pokemon) {
+        if (
+            typeof pokemon === 'object') {
+            pokemonList.push(pokemon);
+        } else {
+            console.log('Error!')
+        }
+    }
 
-//Chain conditional for pokemon height
+    return {
+        getAll: getAll,
+        add: add
+    }
+})();
+
+// Addition of new pokemon item to list
+pokemonRepository.add({
+    name: 'Bulbasaur', height: 2.04, types: ['Grass', ' Poison']
+});
+
+// Print all pokemons in list
+pokemonRepository.getAll().forEach(function (pokemon) {
+    // chain conditional for pokemon height if >0.5
     if (pokemon.height > 0.5) {
-        document.write("<p>" + pokemon.name + ' (height: ' + pokemon.height + ' - Wow, that\'s big!!) ' + ' ' + pokemon.types + "</p>");
+        document.write("<p>" + pokemon.name + ' (height: ' + pokemon.height + ')' + ' - Wow, that\'s big!!' + ' ' + pokemon.types + "</p>");
     } else {
         document.write("<p>" + pokemon.name + ' (height: ' + pokemon.height + ')' + ' ' + pokemon.types + "</p>");
     }
 });
 
-   
+
+
+
+

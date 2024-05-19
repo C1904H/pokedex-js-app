107b5ca56cfd716c64c7ba721ecd3a6c969bfaf8
C1904H <164913030+C1904H@users.noreply.github.com>
2024-05-19 17:29:44 +0100

    Created addListItem() and event listener

--- js/scripts.js
+++ js/scripts.js
@@ -34,9 +34,30 @@ let pokemonRepository = (function () {
         }
     }
 
+// Add list item and button to DOM 
+function addListItem(pokemon){
+    let pokemonList = document.querySelector ('.pokemon-list');
+    let listpokemon = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add("button-class");
+    listpokemon.appendChild(button);
+    pokemonList.appendChild(listpokemon);
+    
+    // Add event listener to button
+    button.addEventListener('click', function () {
+      showDetails(pokemon);
+    });
+  }
+
+  function showDetails(pokemon){
+    console.log(pokemon);
+  }
+
     return {
         getAll: getAll,
-        add: add
+        add: add,
+        addListItem: addListItem
     }
 })();
 
@@ -46,14 +67,9 @@ pokemonRepository.add({
 });
 
 // Print all pokemons in list
-pokemonRepository.getAll().forEach(function (pokemon) {
-    // chain conditional for pokemon height if >0.5
-    if (pokemon.height > 0.5) {
-        document.write("<p>" + pokemon.name + ' (height: ' + pokemon.height + ')' + ' - Wow, that\'s big!!' + ' ' + pokemon.types + "</p>");
-    } else {
-        document.write("<p>" + pokemon.name + ' (height: ' + pokemon.height + ')' + ' ' + pokemon.types + "</p>");
-    }
-});
+pokemonRepository.getAll().forEach(function(pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
 
 
 
